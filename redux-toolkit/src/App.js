ca52e8f7b0a887a9bcc070c2b404e99ea9f48b56
ygmurrsimsek <ygmurrsimsek@icloud.com>
2024-10-13 14:14:29 +0300

    redux ile sayıyı arttırıp azaltma

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { useSelector} from 'react-redux'
+import { useSelector,useDispatch} from 'react-redux'
+import { increment,description } from './redux/CounterSlice';
 function App() {
+  //useSelector statelere .
+  const dispatch=useDispatch();
   const {value}=useSelector((store)=>store.counter);
   console.log(value);
   return (
     <div>
+      <div>{value}</div>
+      <div>
+        <button type="button" onClick={()=>{dispatch(increment())}}>Arttir</button>
+        <button type='button' onClick={()=>{dispatch(description())}}>Azalt</button>
 
-      redux
+      </div>
     </div>
   );
 }
--- redux-toolkit/src/redux/CounterSlice.jsx
+++ redux-toolkit/src/redux/CounterSlice.jsx
@@ -1,16 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+//buraya stateler gelir.useSelector ile statelere erişiriz.
 const initialState = {
-  value: 55,
+  value: 55,//bu bir state ve aşağıya state olarak gider fonksiyonda kullanılacaksa
 }
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
-  reducers: {}
+  //buraya fonksiyonlar gelir ve useDispatch ile çağırılır.birden fazla fonksiyon oluşturabiliriz.
+  reducers: {
+    increment:(state)=>
+    {
+      //yukarıda bulunan 55 değeri state olarak geldi.
+      state.value=state.value+1;
+    },
+    description:(state)=>
+    {
+      state.value=state.value-1;
+    }
+  }
 })
 
 // Action creators are generated for each case reducer function
-export const {} = counterSlice.actions
+export const {increment,description} = counterSlice.actions
 
 export default counterSlice.reducer
