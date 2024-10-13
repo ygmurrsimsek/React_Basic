65a3e045433ddc610091ec5146f1f879f7b51825
ygmurrsimsek <ygmurrsimsek@icloud.com>
2024-10-13 20:29:46 +0300

    Routes ve route bileşenleri oluşturma

--- react_dom/src/App.js
+++ react_dom/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom';
+import Home from './page/Home';
+import About from './page/About';
+import Contact from './page/Contact';
+import Product from './page/Product';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Product' element={<Product />} />
+      <Route path='/Contact' element={<Contact />} />
+    </Routes>
+      
+   </div>
   );
 }
 
