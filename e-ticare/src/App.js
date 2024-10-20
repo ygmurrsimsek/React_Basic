576ee0e076c2c91c34008b0931889bc6c2db8c12
ygmurrsimsek <ygmurrsimsek@icloud.com>
2024-10-20 13:34:34 +0300

    header componentini oluşturuyorum

--- e-ticare/src/App.js
+++ e-ticare/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import Header from './components/Header.jsx';
 function App() {
+  //container ve box clasının altına koyulan tüm kodlar böylelikle sağdan ve soldan boşluk bırakarak yazılır.
   return (
     <div>
-
+      <div className='container'>
+        <div className='box'>
+          <Header/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ e-ticare/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import resim from '../images/logo1.png';
+function Header() {
+  return (
+    <div>
+      <div>
+        <img src={resim} alt="cicek"/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
