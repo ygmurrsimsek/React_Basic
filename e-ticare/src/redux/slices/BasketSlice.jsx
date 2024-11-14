9978bb7c99c1f1bfaeb00e5c6011c76bb02bab2c
ygmurrsimsek <ygmurrsimsek@icloud.com>
2024-11-14 22:13:14 +0300

    sepetteki değişikliklerin ayarlanması ve sepetten çıkınca sepetin içinin boşalmaması için localStorageye yazdırma işlemlerini tanımladık.

--- e-ticare/src/redux/slices/BasketSlice.jsx
+++ e-ticare/src/redux/slices/BasketSlice.jsx
@@ -1,8 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+
+const loadFromLocalStorage = () => {
+    //eğer local storagede basket adında bir veri yazılmışsa diyoruz.
+    if(localStorage.getItem('basket')){
+        return JSON.parse(localStorage.getItem('basket'));
+    }
+    return [];//eğer yazılmamışsa sepette ürün yoktur ve bu da boş bir dizi demektir sepet boş anlamında.
+}
 const initialState =({
-    products:[],
+    products:loadFromLocalStorage(),
 })
+
+const writeLocalStorage = (basket) => {
+    localStorage.setItem('basket', JSON.stringify(basket));//basket adı altında sepete eklenen verileri local storageye ekleyecek.
+}
+
+
 
 const basketSlice= createSlice({
     name:'basket',
