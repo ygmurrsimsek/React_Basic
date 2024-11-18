04478f744f26ced231609694c3934a8b9dc2fced
ygmurrsimsek <ygmurrsimsek@icloud.com>
2024-11-18 13:21:17 +0300

    Formik-Yup eğitimine başladım ve kurulumları gerçekleştirdim.

---
+++ formik-yup/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RegisterForm from "./components/RegisterForm";
+function App() {
+  return (
+    <div>
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ formik-yup/src/components/RegisterForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function RegisterForm() {
+  return (
+    <div>
+      <form>
+        <label>Email</label>
+        <input type='text' id='email' placeholder='Email giriniz'></input>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterForm
