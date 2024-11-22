588b602cdead593b52c23d2a6c6000a5d946de4c
ygmurrsimsek <ygmurrsimsek@icloud.com>
2024-11-22 21:52:58 +0300

    firebase uygulaması öğrenildi

--- my--frabese/src/pages/Auth.jsx
+++ my--frabese/src/pages/Auth.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { FaGoogle } from "react-icons/fa";
 function Auth() {
   return (
     <div className='auth'>
@@ -9,9 +9,12 @@ function Auth() {
         <input type='password' placeholder='Şifre giriniz.' className='input'></input>
       </div>
       <div className='button-div'>
+        
         <button className='button'>Giriş Yap</button>
         <button className='button'>Kayıt Ol</button>
-
+      </div>
+      <div className='button-div'>
+      <button className='button-google'> <FaGoogle className='icons' />Google İle Giriş </button>
       </div>
     </div>
   )
